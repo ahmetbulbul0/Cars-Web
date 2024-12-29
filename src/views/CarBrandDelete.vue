ce58fe3a4d4278218ec6d82901059bfee0a1012f
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2024-12-29 17:08:48 +0300

    car brand delete and edit car pages updated

--- src/views/CarBrandDelete.vue
+++ src/views/CarBrandDelete.vue
@@ -17,23 +17,23 @@ export default {
     methods: {
         fetchData() {
             const carBrandId = this.$route.params.carBrandId;
-            axios.get('http://127.0.0.1:8000/api/car-brand/' + carBrandId)
+            axios.get(`http://127.0.0.1:8000/api/car-brand/${carBrandId}`)
                 .then(response => {
                     this.item = response.data.item;
-                    this.fetchedData = true
+                    this.fetchedData = true;
                 })
                 .catch(error => {
-                    console.error(error);
+                    console.error("Error fetching car brand:", error);
                 });
         },
         deleteItem() {
-            axios.delete('http://127.0.0.1:8000/api/car-brand/' + this.item.id)
-                .then(response => {
+            axios.delete(`http://127.0.0.1:8000/api/car-brand/${this.item.id}`)
+                .then(() => {
                     store.commit("setCarBrandDeletedMessage", this.item.name);
                     this.$router.push({ name: 'CarBrands' });
                 })
                 .catch(error => {
-                    console.error(error);
+                    console.error("Error deleting car brand:", error);
                 });
         },
         cancel() {
@@ -41,9 +41,10 @@ export default {
         },
     },
     components: {
-        Header
+        Header,
     },
 };
+
 </script>
 
 <template>
@@ -52,7 +53,7 @@ export default {
             <Header />
             <div class="w-full flex justify-center mt-6" v-if="fetchedData == false">
                 <div class="w-1/2 bg-gray-50 rounded-md p-4 flex justify-center items-center h-24">
-                    <img class="h-12 w-12" src="../assets/gif/1488.gif" alt="">
+                    <img class="h-12 w-12" src="../assets/gif/1488.gif" alt="Loading...">
                 </div>
             </div>
             <div class="w-full flex justify-center mt-6" v-if="item != null">
@@ -61,13 +62,13 @@ export default {
                         <span>Are You Sure?</span>
                     </div>
                     <div class="w-full flex justify-center items-center text-sm font-medium text-gray-600">
-                        <span>if you confirm, deleting "{{ item.name }}" named car brand</span>
+                        <span>If you confirm, "{{ item.name }}" car brand will be deleted.</span>
                     </div>
                     <div class="w-full text-sm font-medium flex flex-col items-center gap-y-2 mt-2">
-                        <button type="submit"
+                        <button type="button"
                             class="w-2/3 px-4 py-2 rounded-l-sm bg-red-700 text-white font-medium hover:bg-red-600"
                             @click="deleteItem()">Confirm, Delete</button>
-                        <button type="submit"
+                        <button type="button"
                             class="w-2/3 px-4 py-2 rounded-r-sm bg-gray-200 text-gray-500 font-medium hover:bg-gray-300 hover:text-gray-600"
                             @click="cancel()">Cancel, Don't Delete</button>
                     </div>
@@ -75,4 +76,4 @@ export default {
             </div>
         </div>
     </div>
-</template>q
+</template>
--- src/views/EditCar.vue
+++ src/views/EditCar.vue
@@ -1,5 +1,4 @@
 <script>
-
 import axios from 'axios';
 import Header from './components/Header.vue';
 import { ref } from 'vue';
@@ -11,28 +10,49 @@ export default {
             fetchedData: false,
             carTypes: null,
             carBrands: null,
-            name: ref(null),
+            carFeatures: null,
+            modelName: ref(null),
             type: ref(0),
-            brand: ref(0)
+            brand: ref(0),
+            productionYear: ref(null),
+            color: ref(null),
+            engineType: ref(null),
+            horsepower: ref(null),
+            torque: ref(null),
+            transmission: ref(null),
+            fuelConsumption: ref(null),
+            price: ref(null),
+            selectedFeatures: [], // Seçilen özellikleri tutar
         };
     },
     mounted() {
         this.fetchData();
         this.fetchCarTypes();
         this.fetchCarBrands();
+        this.fetchCarFeatures();
     },
     methods: {
         fetchData() {
             const carId = this.$route.params.carId;
-            axios.get('http://127.0.0.1:8000/api/car/' + carId)
+            axios.get(`http://127.0.0.1:8000/api/car/${carId}`)
                 .then(response => {
-                    this.name = response.data.item.name;
-                    this.type = response.data.item.type.id;
-                    this.brand = response.data.item.brand.id;
+                    const item = response.data.item;
+                    this.modelName = item.modelName;
+                    this.type = item.type.id;
+                    this.brand = item.brand.id;
+                    this.productionYear = item.productionYear;
+                    this.color = item.color;
+                    this.engineType = item.engineType;
+                    this.horsepower = item.horsepower;
+                    this.torque = item.torque;
+                    this.transmission = item.transmission;
+                    this.fuelConsumption = item.fuelConsumption;
+                    this.price = item.price;
+                    this.selectedFeatures = item.features.map(feature => feature.id); // Özellikleri ID olarak al
                     this.fetchedData = true;
                 })
                 .catch(error => {
-                    console.error(error);
+                    console.error("Error fetching car data:", error);
                 });
         },
         fetchCarTypes() {
@@ -41,7 +61,7 @@ export default {
                     this.carTypes = response.data.data;
                 })
                 .catch(error => {
-                    console.error(error);
+                    console.error("Error fetching car types:", error);
                 });
         },
         fetchCarBrands() {
@@ -50,76 +70,89 @@ export default {
                     this.carBrands = response.data.data;
                 })
                 .catch(error => {
-                    console.error(error);
+                    console.error("Error fetching car brands:", error);
+                });
+        },
+        fetchCarFeatures() {
+            axios.get('http://127.0.0.1:8000/api/car-feature')
+                .then(response => {
+                    this.carFeatures = response.data.data;
+                })
+                .catch(error => {
+                    console.error("Error fetching car features:", error);
                 });
         },
         update() {
             this.resetFormInputErrors();
 
-            var carName = this.name;
-            var carType = this.type
-            var carBrand = this.brand;
+            const carData = {
+                modelName: this.modelName,
+                type: this.type,
+                brand: this.brand,
+                productionYear: this.productionYear,
+                color: this.color,
+                engineType: this.engineType,
+                horsepower: this.horsepower,
+                torque: this.torque,
+                transmission: this.transmission,
+                fuelConsumption: this.fuelConsumption,
+                price: this.price,
+                features: this.selectedFeatures, // Seçilen özellikleri gönder
+            };
 
-            if (carName && carType != 0 && carBrand != 0) {
-                const carId = this.$route.params.carId;
-                axios.patch(`http://127.0.0.1:8000/api/car/${carId}`, {
-                    name: carName,
-                    type: carType,
-                    brand: carBrand
-                }).then(response => {
+            const carId = this.$route.params.carId;
+
+            axios.patch(`http://127.0.0.1:8000/api/car/${carId}`, carData)
+                .then(response => {
                     store.commit("setCarUpdatedMessage");
                     this.$router.push({ name: 'CarDetail', params: { carId: response.data.updated_old.id } });
-                }).catch(error => {
-                    var errors = error.response.data.errors;
-                    if (errors.name) {
-                        document.querySelector("[name='name']").classList.remove("border-gray-300");
-                        document.querySelector("[name='name']").classList.add("border-red-700");
-
-                        let box = document.querySelector("[name='name']").parentNode.parentNode;
-                        var errorsContent = `<div class="w-full flex justify-end items-right flex-col gap-y-2 mt-2 text-sm text-right text-red-700" id="nameErrorBox">`;
-
-                        errors.name.forEach(nameError => {
-                            errorsContent = errorsContent + `<span>${nameError}</span>`;
-                        });
-
-                        errorsContent = errorsContent + "</div>";
-                        box.innerHTML = box.innerHTML + errorsContent;
+                })
+                .catch(error => {
+                    const errors = error.response.data.errors;
+                    for (const field in errors) {
+                        this.handleFieldError(field, errors[field]);
                     }
                 });
-            }
-
-            if (!carName) {
-                document.querySelector("[name='name']").classList.remove("border-gray-300");
-                document.querySelector("[name='name']").classList.add("border-red-700");
-            }
-
-            if (carType == 0) {
-                document.querySelector("[name='type']").classList.remove("border-gray-200");
-                document.querySelector("[name='type']").classList.add("border-red-700");
-            }
-
-            if (carBrand == 0) {
-                document.querySelector("[name='brand']").classList.remove("border-gray-200");
-                document.querySelector("[name='brand']").classList.add("border-red-700");
-            }
-
         },
         resetFormInputErrors() {
-            document.querySelector("#nameErrorBox") ? document.querySelector("#nameErrorBox").remove() : null;
-            document.querySelector("[name='name']").classList.add("border-gray-300");
-            document.querySelector("[name='name']").classList.remove("border-red-700");
-            document.querySelector("[name='type']").classList.add("border-gray-200");
-            document.querySelector("[name='type']").classList.remove("border-red-700");
-            document.querySelector("[name='brand']").classList.add("border-gray-200");
-            document.querySelector("[name='brand']").classList.remove("border-red-700");
-        }
+            const fields = ["name", "type", "brand", "productionYear", "color", "engineType", "horsepower", "torque", "transmission", "fuelConsumption", "price"];
+            fields.forEach(field => {
+                const errorBox = document.querySelector(`#${field}ErrorBox`);
+                if (errorBox) errorBox.remove();
+
+                const inputElement = document.querySelector(`[name='${field}']`);
+                if (inputElement) {
+                    inputElement.classList.add("border-gray-300");
+                    inputElement.classList.remove("border-red-700");
+                }
+            });
+        },
+        handleFieldError(field, errorMessages) {
+            const inputElement = document.querySelector(`[name='${field}']`);
+            if (inputElement) {
+                inputElement.classList.remove("border-gray-300");
+                inputElement.classList.add("border-red-700");
+
+                const box = inputElement.parentNode.parentNode;
+                const errorBoxId = `${field}ErrorBox`;
+
+                if (!document.querySelector(`#${errorBoxId}`)) {
+                    let errorsContent = `<div class="w-full flex justify-end items-right flex-col gap-y-2 mt-2 text-sm text-right text-red-700" id="${errorBoxId}">`;
+                    errorMessages.forEach(errorMessage => {
+                        errorsContent += `<span>${errorMessage}</span>`;
+                    });
+                    errorsContent += "</div>";
+                    box.innerHTML += errorsContent;
+                }
+            }
+        },
     },
     components: {
-        Header
+        Header,
     },
 };
-
 </script>
+
 
 <template>
     <div class="w-full h-full flex justify-center items-center my-6 px-6">
@@ -127,42 +160,134 @@ export default {
             <Header />
             <div class="w-full flex justify-center mt-6" v-if="fetchedData == false">
                 <div class="w-1/2 bg-gray-50 rounded-md p-4 flex justify-center items-center h-24">
-                    <img class="h-12 w-12" src="../assets/gif/1488.gif" alt="">
+                    <img class="h-12 w-12" src="../assets/gif/1488.gif" alt="Loading...">
                 </div>
             </div>
             <div class="w-full flex justify-center mt-6" v-if="fetchedData == true">
                 <div class="w-1/2 bg-gray-50 rounded-md p-4 flex flex-col gap-y-4">
+                    <!-- Model Name -->
                     <div>
                         <div class="w-full flex justify-start items-center gap-4">
-                            <label class="font-bold text-gray-600 w-1/6">Name:</label>
-                            <input name="name" v-model="name" type="text"
+                            <label class="font-bold text-gray-600 w-1/6">Model Name:</label>
+                            <input name="modelName" v-model="modelName" type="text"
                                 class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
-                                placeholder="Enter Something...">
+                                placeholder="Enter Model Name...">
                         </div>
                     </div>
+                    <!-- Type -->
                     <div>
                         <div class="w-full flex justify-start items-center gap-4">
                             <label class="font-bold text-gray-600 w-1/6">Type:</label>
                             <select name="type" v-model="type"
                                 class="w-5/6 px-4 py-2 rounded-sm bg-gray-200 border border-gray-200">
                                 <option value="0" disabled>Choose Type</option>
-                                <option v-for="carType in carTypes" :key="carType.id" :value="carType.id">{{ carType.name }}
+                                <option v-for="carType in carTypes" :key="carType.id" :value="carType.id">
+                                    {{ carType.name }}
                                 </option>
                             </select>
                         </div>
                     </div>
+                    <!-- Brand -->
                     <div>
                         <div class="w-full flex justify-start items-center gap-4">
                             <label class="font-bold text-gray-600 w-1/6">Brand:</label>
                             <select name="brand" v-model="brand"
                                 class="w-5/6 px-4 py-2 rounded-sm bg-gray-200 border border-gray-200">
                                 <option value="0" disabled>Choose Brand</option>
-                                <option v-for="carBrand in carBrands" :key="carBrand.id" :value="carBrand.id">{{
-                                    carBrand.name
-                                }}</option>
+                                <option v-for="carBrand in carBrands" :key="carBrand.id" :value="carBrand.id">
+                                    {{ carBrand.name }}
+                                </option>
                             </select>
                         </div>
                     </div>
+                    <!-- Features -->
+                    <div>
+                        <div class="w-full flex flex-col gap-y-2">
+                            <label class="font-bold text-gray-600">Features:</label>
+                            <div class="grid grid-cols-2 gap-2">
+                                <div v-for="feature in carFeatures" :key="feature.id">
+                                    <label class="flex items-center gap-x-2">
+                                        <input type="checkbox" :value="feature.id" v-model="selectedFeatures"
+                                            class="form-checkbox">
+                                        <span>{{ feature.name }}</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Production Year -->
+                    <div>
+                        <div class="w-full flex justify-start items-center gap-4">
+                            <label class="font-bold text-gray-600 w-1/6">Production Year:</label>
+                            <input name="productionYear" v-model="productionYear" type="number"
+                                class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
+                                placeholder="Enter Production Year">
+                        </div>
+                    </div>
+                    <!-- Color -->
+                    <div>
+                        <div class="w-full flex justify-start items-center gap-4">
+                            <label class="font-bold text-gray-600 w-1/6">Color:</label>
+                            <input name="color" v-model="color" type="text"
+                                class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
+                                placeholder="Enter Color">
+                        </div>
+                    </div>
+                    <!-- Engine Type -->
+                    <div>
+                        <div class="w-full flex justify-start items-center gap-4">
+                            <label class="font-bold text-gray-600 w-1/6">Engine Type:</label>
+                            <input name="engineType" v-model="engineType" type="text"
+                                class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
+                                placeholder="Enter Engine Type">
+                        </div>
+                    </div>
+                    <!-- Horsepower -->
+                    <div>
+                        <div class="w-full flex justify-start items-center gap-4">
+                            <label class="font-bold text-gray-600 w-1/6">Horsepower:</label>
+                            <input name="horsepower" v-model="horsepower" type="number"
+                                class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
+                                placeholder="Enter Horsepower">
+                        </div>
+                    </div>
+                    <!-- Torque -->
+                    <div>
+                        <div class="w-full flex justify-start items-center gap-4">
+                            <label class="font-bold text-gray-600 w-1/6">Torque:</label>
+                            <input name="torque" v-model="torque" type="number"
+                                class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
+                                placeholder="Enter Torque">
+                        </div>
+                    </div>
+                    <!-- Transmission -->
+                    <div>
+                        <div class="w-full flex justify-start items-center gap-4">
+                            <label class="font-bold text-gray-600 w-1/6">Transmission:</label>
+                            <input name="transmission" v-model="transmission" type="text"
+                                class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
+                                placeholder="Enter Transmission">
+                        </div>
+                    </div>
+                    <!-- Fuel Consumption -->
+                    <div>
+                        <div class="w-full flex justify-start items-center gap-4">
+                            <label class="font-bold text-gray-600 w-1/6">Fuel Consumption:</label>
+                            <input name="fuelConsumption" v-model="fuelConsumption" type="number"
+                                class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
+                                placeholder="Enter Fuel Consumption (L/100km)">
+                        </div>
+                    </div>
+                    <!-- Price -->
+                    <div>
+                        <div class="w-full flex justify-start items-center gap-4">
+                            <label class="font-bold text-gray-600 w-1/6">Price:</label>
+                            <input name="price" v-model="price" type="number"
+                                class="w-5/6 px-4 py-2 rounded-sm outline-none border border-gray-300"
+                                placeholder="Enter Price">
+                        </div>
+                    </div>
+                    <!-- Submit Button -->
                     <div class="w-full flex justify-start items-center gap-4">
                         <button type="submit" class="w-full px-4 py-2 rounded-sm bg-gray-200 text-gray-700 font-medium"
                             @click="update()">Update</button>
